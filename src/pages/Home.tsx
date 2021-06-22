d7e1a1f1ef3dfc0e7f159219a5daa6fc856b1ce9
agftech <agiraof@gmail.com>
2021-06-22 16:38:35 -0700

    :art: style: add style to the home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,20 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss';
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="letmeask" />
-          <button>
+          <button className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
