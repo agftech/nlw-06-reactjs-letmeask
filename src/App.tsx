e90f79ce12469e2a52c8f42553178e328783d1cb
agftech <agiraof@gmail.com>
2021-06-23 10:56:00 -0700

    :package: feat: add newRoom route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 export default function App() {
   return (
     <BrowserRouter>
         <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
     </BrowserRouter>
   );
 }
