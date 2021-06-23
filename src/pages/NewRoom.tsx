2ac174856ad2a02115b65e3408a110125cf3f343
agftech <agiraof@gmail.com>
2021-06-23 11:05:19 -0700

    :package: feat: add navigation

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
@@ -27,7 +29,7 @@ export function NewRoom() {
             </Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
           </p>
         </div>
       </main>
