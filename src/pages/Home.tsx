834d39bbf2eb9dea9e55bc383cfeef28fdb16cad
agftech <agiraof@gmail.com>
2021-06-23 09:45:28 -0700

    :package: feat: add button component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { Button } from '../components/Button';
+
 import '../styles/auth.scss';
 
 export function Home() {
@@ -25,9 +27,9 @@ export function Home() {
               type="text"
               placeholder="Digite o código da sala"
             />
-            <button type="submit">
+            <Button type="submit">
               Entrar na sala
-            </button>
+            </Button>
           </form>
         </div>
       </main>      
