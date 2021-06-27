28758ca499d89bba3b55d7b11ae9e2c3b6bda783
agftech <agiraof@gmail.com>
2021-06-26 17:01:06 -0700

    :package: feat: add close room

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from 'react-router-dom'
 
 import logoImg from "../assets/images/logo.svg";
 import deleteImg from '../assets/images/delete.svg';
@@ -16,13 +16,21 @@ type RoomParams = {
 };
 
 export function AdminRoom() {
+  const history = useHistory()
   const params = useParams<RoomParams>();
-
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId);
 
-    async function handleDeleteQuestion(questionId: string) {
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      closedAt: new Date(),
+    })
+
+    history.push('/');
+  }
+
+  async function handleDeleteQuestion(questionId: string) {
     if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
@@ -35,7 +43,7 @@ export function AdminRoom() {
           <img src={logoImg} alt="Letmeask" />
           <div>
             <RoomCode code={roomId} />
-            <Button isOutlined>Encerrar sala</Button>
+			<Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
           </div>      
         </div>
       </header>
